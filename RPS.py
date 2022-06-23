1261fe06b7d053e0085d4020469748ec608a293f
JoaoBRBR <drvictor321@gmail.com>
2022-06-23 10:43:48 -0300

    Inicio Falho

--- RPS.py
+++ RPS.py
@@ -1,1 +1,19 @@
-hhhh
+import random
+#implementação da logica simples, pode mudar como quiser
+
+
+#variaveis que guardam jogada do player e bot
+player = 0
+bot = 0
+
+print('escolha pedra1 papel2 tesoura3')
+
+input(player)
+
+bot = random.choice('1','2','3')
+
+print(player)
+
+# nesse codigo era pra escolher um numero, o but escolher 
+# aleatoriamente, mas nao deu.
+# pode resolver isso ai ;-;
