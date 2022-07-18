a02c9926a6cd83af590911558912f2db6d37d175
JoaoBRBR <drvictor321@gmail.com>
2022-07-18 10:15:56 -0300

    Parte de botoes organizada

--- Jogo.py
+++ Jogo.py
@@ -117,96 +117,35 @@ while True:
                 quit()
         # erro: criar variavel de rodada para ficar duas e não só uma
         if event.type == pygame.MOUSEBUTTONUP:
+            #botao menu
             if Menu == True and x > 240 and x < 360 and y > 250 and y < 366:
                 Menu = False
                 #inicia musica 
                 Menu_music.stop()
                 pygame.mixer.music.play(-1)
-
+            #logica dos botoes
             if modoJogo == True:
-                    
                 if x < 190 and x > 80 and y > 440 and y < 580 and blockPedra < 3:
                     button.play()
                     blockPedra, blockPapel, blockTesoura, player, modoJogo, chave = aba.OpPedra(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave)
                     
                 elif x < 340 and x > 250 and y > 440 and y < 580 and blockPapel < 3:
                     button.play()
-                    blockPapel += 1
-                    if blockPedra == 2:
-                        blockPedra -= 1
-                    if blockTesoura == 2:
-                        blockTesoura -= 1
-                    player = 2
-                    modoJogo = False
-                    chave = False
-                    if blockPedra == 3:
-                        blockPedra += 1
-                    elif blockPedra == 4:
-                        blockPedra = 1
-                    if blockTesoura == 3:
-                        blockTesoura += 1
-                    elif blockTesoura == 4:
-                        blockTesoura = 1
+                    blockPedra, blockPapel, blockTesoura, player, modoJogo, chave = aba.OpPapel(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave)
                     
                 elif x < 490 and x > 390 and y > 440 and y < 580 and blockTesoura < 3:
                     button.play()
-                    blockTesoura += 1
-                    if blockPedra == 2:
-                        blockPedra -= 1
-                    if blockPapel == 2:
-                        blockPapel -= 1
-                    player = 3
-                    modoJogo = False
-                    chave = False
-                    if blockPedra == 3:
-                        blockPedra += 1
-                    elif blockPedra == 4:
-                        blockPedra = 1
-                    if blockPapel == 3:
-                        blockPapel += 1
-                    elif blockPapel == 4:
-                        blockPapel = 1
+                    blockPedra, blockPapel, blockTesoura, player, modoJogo, chave = aba.OpTesoura(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave)
                         
             # reset do jogo
             if Fim == True:
                 #voltar pro menu
                 if x < 428 and x > 313 and y > 300 and y < 415:
-                    Menu = True
                     pygame.mixer.music.stop()
-                    player = 0
-                    bot = 0
-                    blockPedra = 1
-                    blockPapel = 1
-                    blockTesoura = 1
-                    playerVida = 200
-                    botVida = 200
-                    modoJogo = True
-                    chave = True
-                    chaveA = 0
-                    botKey = False
-                    vidaKey = False
-                    Fim = False
-                    py = 150
-                    pv = 3
-                    by = 150
+                    Menu,player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by = aba.OpVoltaMenu(Menu,player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by)
                 # jogar de novo 
                 if x < 286 and x > 170 and y > 300 and y < 415:
-                    player = 0
-                    bot = 0
-                    blockPedra = 1
-                    blockPapel = 1
-                    blockTesoura = 1
-                    playerVida = 200
-                    botVida = 200
-                    modoJogo = True
-                    chave = True
-                    chaveA = 0
-                    botKey = False
-                    vidaKey = False
-                    Fim = False
-                    py = 150
-                    pv = 3
-                    by = 150        
+                    player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by = aba.OpReplay(player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by)        
     # reloading da tela
     screen.fill((0, 0, 0))
     
--- aba.py
+++ aba.py
@@ -1,3 +1,5 @@
+#--------------------aba contem logicas dos botoes------------------------#
+#logica botao pedra
 def OpPedra(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave):
     blockPedra += 1
     if blockPapel == 2:
@@ -18,3 +20,80 @@ def OpPedra(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave):
     elif blockPapel == 4:
         blockPapel = 1
     return blockPedra, blockPapel, blockTesoura, player, modoJogo, chave
+#logica botao papel
+def OpPapel(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave):
+    blockPapel += 1
+    if blockPedra == 2:
+        blockPedra -= 1
+    if blockTesoura == 2:
+        blockTesoura -= 1
+    player = 2
+    modoJogo = False
+    chave = False
+    if blockPedra == 3:
+        blockPedra += 1
+    elif blockPedra == 4:
+        blockPedra = 1
+    if blockTesoura == 3:
+        blockTesoura += 1
+    elif blockTesoura == 4:
+        blockTesoura = 1
+    return blockPedra, blockPapel, blockTesoura, player, modoJogo, chave
+#logica botao tesoura
+def OpTesoura(blockPedra, blockPapel, blockTesoura, player, modoJogo, chave):
+    blockTesoura += 1
+    if blockPedra == 2:
+        blockPedra -= 1
+    if blockPapel == 2:
+        blockPapel -= 1
+    player = 3
+    modoJogo = False
+    chave = False
+    if blockPedra == 3:
+        blockPedra += 1
+    elif blockPedra == 4:
+        blockPedra = 1
+    if blockPapel == 3:
+        blockPapel += 1
+    elif blockPapel == 4:
+        blockPapel = 1
+    return blockPedra, blockPapel, blockTesoura, player, modoJogo, chave
+#logica botao volar para menu
+def OpVoltaMenu(Menu,player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by):
+    Menu = True
+    player = 0
+    bot = 0
+    blockPedra = 1
+    blockPapel = 1
+    blockTesoura = 1
+    playerVida = 200
+    botVida = 200
+    modoJogo = True
+    chave = True
+    chaveA = 0
+    botKey = False
+    vidaKey = False
+    Fim = False
+    py = 150
+    pv = 3
+    by = 150
+    return Menu,player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by
+#logica botao jogar novamente
+def OpReplay(player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by):
+    player = 0
+    bot = 0
+    blockPedra = 1
+    blockPapel = 1
+    blockTesoura = 1
+    playerVida = 200
+    botVida = 200
+    modoJogo = True
+    chave = True
+    chaveA = 0
+    botKey = False
+    vidaKey = False
+    Fim = False
+    py = 150
+    pv = 3
+    by = 150
+    return player,bot,blockPedra,blockPapel,blockTesoura,playerVida,botVida,modoJogo,chave,chaveA,botKey,vidaKey,Fim,py,pv,by
